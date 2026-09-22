f4b17e5d9bfa89ac567a07c01c791a214331ebfb
AryanYadav <aryan.yofficial@gmail.com>
2026-09-22 20:08:53 +0530

    Initial commit

---
+++ introduction.java
@@ -0,0 +1,8 @@
+public class introduction {
+    public static void main(String[] args){
+        System.out.println("Hey buddy, what's up!");
+        System.out.println("Have you eaten something?");
+        System.out.println("How about pizza or maybe burger??");
+        // sout shortcut to write system out print
+    }   // println to print in next line 
+}
